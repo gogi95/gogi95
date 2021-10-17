f7760f319ea2a2a74ba4450b00631cc96dd95e84
gogi95 <92687762+gogi95@users.noreply.github.com>
2021-10-17 20:01:48 +0200

    Create on-click.js

---
+++ on-click.js
@@ -0,0 +1,20 @@
+var textOverImages = document.getElementsByClassName(".image-overlay");
+var previousTextOverImage;
+for (var i = 0; i < textOverImages.length; i++) {
+  textOverImages[i].onclick = function() {
+    var classes = this.classList;
+    if (classes.contains("show")) {
+      classes.remove("show");
+    } else {
+      if (previousTextOverImage != null)
+        previousTextOverImage.classList.remove("show");
+      previousTextOverImage = this;
+      classes.add("show");
+    }
+  }
+}
+
+function stopPropagation(event){
+  event.stopPropagation();
+  
+}
